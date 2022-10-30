cfc99d7bc8820658f5c985cb5b0b78b247a7fd3b
AlfaLucky <shiryaaa@mail.ru>
2022-10-30 12:37:56 +0600

    HowManyMiles
        # Calculate and output the number of miles in 1000 kilometers.
        # Hint: One mile is 1.6 kilometers, so find the quotient of 1000 and 1.6.
        # Use the floor division operator //.

---
+++ How_many_miles/HowManyMiles.py
@@ -0,0 +1,11 @@
+class HowManyMiles:
+    # empty constructor
+    def __init__(self):
+        pass
+
+    # Calculate and output the number of miles in 1000 kilometers.
+    # Hint: One mile is 1.6 kilometers, so find the quotient of 1000 and 1.6.
+    # Use the floor division operator //.
+    def division_operator(self, x1, x2):
+        print(x1 // x2)
+        return x1 // x2
