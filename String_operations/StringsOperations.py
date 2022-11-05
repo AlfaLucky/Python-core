0bfcbd58b5b21389db8f63f2acc6f32a3abc20ec
AlfaLucky <shiryaaa@mail.ru>
2022-11-05 18:52:01 +0600

    multiplication, string addition

---
+++ String_operations/StringsOperations.py
@@ -0,0 +1,10 @@
+class StringsOperations:
+    # empty constructor
+    def __init__(self):
+        pass
+
+    # add method - given two numbers, return the addition
+    def strings_operations(self, x1):
+        x1 = (x1 * 3) + "!"
+        print(x1)
+        return x1
