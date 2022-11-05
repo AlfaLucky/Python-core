e59e37f0ba484dd62ec7f431a42fb9d70a45f84b
AlfaLucky <shiryaaa@mail.ru>
2022-11-05 19:30:33 +0600

    multiple variables and convert str in int

---
+++ Multiple_Variables/MultipleVariables.py
@@ -0,0 +1,10 @@
+class MultipleVariables:
+    # empty constructor
+    def __init__(self):
+        pass
+
+    # add method - given two numbers, return the addition
+    def multiple_variables(self, x1, x2):
+        x1 = x1 * int(x2)
+        print(x1)
+        return x1
