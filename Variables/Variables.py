324c960b8fc0cd9efa36a93e2bd6e7a4dc15e75d
AlfaLucky <shiryaaa@mail.ru>
2022-11-05 19:09:37 +0600

    exponentiation

---
+++ Variables/Variables.py
@@ -0,0 +1,10 @@
+class Variables:
+    # empty constructor
+    def __init__(self):
+        pass
+
+    # add method - given two numbers, return the addition
+    def Variables(self, x1, x2):
+        x1 = x1**x2
+        print(x1)
+        return x1
